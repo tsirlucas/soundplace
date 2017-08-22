8e4158c43e4788fb92265767071852529c853b20
lucascorreia <tsirlucas@gmail.com>
2017-08-22 01:10:35 -0300

    Using package instead of lighthouse-ci copy and paste

--- lighthouse-ci/builder/docker_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Build for Headless Chrome.
-# docker build -t lighthouse_ci . --build-arg CACHEBUST=$(date +%d)
-
-# Build for non-headless Chrome version.
-docker build -f Dockerfile.nonheadless -t lighthouse_ci . --build-arg CACHEBUST=$(date +%d)
--- lighthouse-ci/builder/docker_run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -d -p 8080:8080 --cap-add=SYS_ADMIN lighthouse_ci
