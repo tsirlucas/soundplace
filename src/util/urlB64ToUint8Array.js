592fa5f4caca4f9d4ee7be9462a830d35ec57d3f
lucascorreia <tsirlucas@gmail.com>
2017-08-21 22:39:52 -0300

    Fixing lint error

--- src/util/urlB64ToUint8Array.js
+++ src/util/urlB64ToUint8Array.js
@@ -1,7 +1,7 @@
 export const urlB64ToUint8Array = (base64String) => {
   const padding = '='.repeat((4 - base64String.length % 4) % 4);
   const base64 = (base64String + padding)
-    .replace(/\-/g, '+')
+    .replace(/-/g, '+')
     .replace(/_/g, '/');
 
   const rawData = window.atob(base64);
