5203b12c052462911b3d1186749e793b0ea58623
lucascorreia <tsirlucas@gmail.com>
2017-09-04 20:29:21 -0300

    Roll back SW config

--- src/util/initSW.js
+++ src/util/initSW.js
@@ -16,31 +16,18 @@ function registerSuccess(reg) {
 
 function onUpdateFound(reg) {
   return () => {
-    console.log('Service worker update found! Removing old cache before install...');
-    clearCache().then(() => {
-      console.log('Done! Updating service worker...');
-      const installingWorker = reg.installing;
+    console.log('Updating service worker...');
+    const installingWorker = reg.installing;
 
-      installingWorker.onstatechange = onStateChange(installingWorker);
-    });
+    installingWorker.onstatechange = onStateChange(installingWorker);
   };
-}
-
-function clearCache() {
-  return caches.keys().then((cacheNames) => {
-    return Promise.all(
-      cacheNames.map((cacheName) => {
-        return caches.delete(cacheName);
-      })
-    );
-  });
 }
 
 function onStateChange(installingWorker) {
   return () => {
     if (installingWorker.state === 'activated' && navigator.serviceWorker && navigator.serviceWorker.controller) {
-      console.log('Service worker updated! Reloading page...');
-      window.location.reload(true);
+      console.log('Done! Reloading page...');
+      window.location.reload();
     }
   };
 }
