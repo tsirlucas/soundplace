628b9a0a63bc8bfe99183d482bb4a7f7dfb47801
lucascorreia <tsirlucas@gmail.com>
2017-09-04 21:41:12 -0300

    Fixing auth

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,13 @@
 import { h, Component } from 'preact';
 import queryString from 'query-string';
-import Cookie from 'js-cookie';
+import Cookies from 'js-cookie';
 
 import Card from '../../components/Card';
 import Button from '../../components/Button';
 import { SPOTIFY_AUTH } from '../../core/api/api.constants';
+import {browserHistory} from '../../routes/routes.config';
+
+const cookies = Cookies.withConverter((value) => value);
 
 export default class Login extends Component {
 
@@ -12,7 +15,8 @@ export default class Login extends Component {
     const { route } = this.context.router;
     const params = queryString.parse(route.location.search);
     if (params.success) {
-      Cookie.set('token', params['access_token'], { secure: true, expires: params['exp'] });
+      cookies.set('token', params['access_token'], { secure: true, expires: params['exp'] });
+      browserHistory.push('page2');
     }
   }
 
