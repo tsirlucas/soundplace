6388bd672e5591ad4ed2a08d99e8576c59510792
Marcelo Silva <marcelosilva.developer@gmail.com>
2017-09-18 12:17:26 -0300

    Fix playlists reducer

--- src/core/playlists/playlists.reducer.js
+++ src/core/playlists/playlists.reducer.js
@@ -3,7 +3,7 @@ import { REQUEST_PLAYLISTS_SUCCESS } from './playlists.constants';
 const PlaylistsReducer = (state = [], action) => {
   switch (action.type) {
     case REQUEST_PLAYLISTS_SUCCESS:
-      return [...state.payload.data];
+      return [...action.payload.response.data];
     default:
       return state;
   }
