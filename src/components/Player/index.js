e9280eb374ee389bf366e1225e3466a8ccc0b735
lucascorreia <tsirlucas@gmail.com>
2017-10-08 18:11:44 -0300

    Fixes on mocked data and player album

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -53,15 +53,8 @@ export default class Player extends Component {
       navigator.mediaSession.metadata = new window.MediaMetadata({
         title: currentlyPlaying.name,
         artist: currentlyPlaying.artist,
-        album: currentlyPlaying.artist,
-        artwork: [
-          { src: 'https://dummyimage.com/96x96', sizes: '96x96', type: 'image/png' },
-          { src: 'https://dummyimage.com/128x128', sizes: '128x128', type: 'image/png' },
-          { src: 'https://dummyimage.com/192x192', sizes: '192x192', type: 'image/png' },
-          { src: 'https://dummyimage.com/256x256', sizes: '256x256', type: 'image/png' },
-          { src: 'https://dummyimage.com/384x384', sizes: '384x384', type: 'image/png' },
-          { src: 'https://dummyimage.com/512x512', sizes: '512x512', type: 'image/png' }
-        ]
+        album: currentlyPlaying.album,
+        artwork: currentlyPlaying.artwork
       });
 
       navigator.mediaSession.setActionHandler('play', this.props.actions.toggle);
--- src/core/player/player.epics.js
+++ src/core/player/player.epics.js
@@ -6,34 +6,47 @@ import { Observable } from '../../util/RXImports';
 import { INIT_PLAYER, PLAYER_CLEAR, PLAYER_DESTROY } from './player.constants';
 import { initPlayerSuccess } from './player.actions';
 
+const mockedArtwork = [
+  { src: 'https://dummyimage.com/96x96', sizes: '96x96', type: 'image/png' },
+  { src: 'https://dummyimage.com/128x128', sizes: '128x128', type: 'image/png' },
+  { src: 'https://dummyimage.com/192x192', sizes: '192x192', type: 'image/png' },
+  { src: 'https://dummyimage.com/256x256', sizes: '256x256', type: 'image/png' },
+  { src: 'https://dummyimage.com/384x384', sizes: '384x384', type: 'image/png' },
+  { src: 'https://dummyimage.com/512x512', sizes: '512x512', type: 'image/png' }
+];
+
 const mockedState = {
   isPlaying: false,
   currentIndex: 0,
   lastCurrentTime: 4.184037,
   currentlyPlaying: {
-    name: 'titulo mockado',
-    artist: 'artista mockado',
-    album: 'album mockado',
+    name: 'All This Is',
+    artist: 'Joe L.\'s Studio',
+    album: 'Some Mocked',
+    artwork: mockedArtwork,
     url: 'https://ia800703.us.archive.org/3/items/mythium/JLS_ATI.mp3'
   },
   tracklist: [
     {
-      name: 'titulo mockado',
-      artist: 'artista mockado',
-      album: 'album mockado',
+      name: 'All This Is',
+      artist: 'Joe L.\'s Studio',
+      album: 'Some Mocked',
+      artwork: mockedArtwork,
       url: 'https://ia800703.us.archive.org/3/items/mythium/JLS_ATI.mp3'
     },
     {
-      name: 'titulo mockado 2',
-      artist: 'artista mockado 2',
-      album: 'album mockado',
+      name: 'The Forsaken',
+      artist: 'Broadwing Studio',
+      album: 'Some Mocked',
+      artwork: mockedArtwork,
       url: 'https://ia800703.us.archive.org/3/items/mythium/BS_TF.mp3'
     },
     {
-      name: 'titulo mockado 3',
-      artist: 'artista mockado 3',
-      album: 'album mockado',
-      url: 'https://ia800703.us.archive.org/3/items/mythium/SSB12_28_03_T.mp3'
+      name: 'On The Waterfront',
+      artist: 'Popeye\'s',
+      album: 'Some Mocked',
+      artwork: mockedArtwork,
+      url: 'https://ia800703.us.archive.org/3/items/mythium/PNY04-05_OTW.mp3'
     }
   ]
 };
