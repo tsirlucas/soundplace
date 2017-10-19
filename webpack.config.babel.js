1bd7267b37deba2106aa4cbedd6e7f3708c75421
lucascorreia <tsirlucas@gmail.com>
2017-10-19 09:28:32 -0300

    Using npm to control sentry version

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,8 +1,8 @@
 import path from 'path';
-import {loaders} from './config/loaders';
-import {plugins} from './config/common.config';
-import {devPlugins, devServerconfig, devLoaders} from './config/dev.config';
-import {prodPlugins, prodLoaders} from './config/prod.config';
+import { loaders } from './config/loaders';
+import { plugins } from './config/common.config';
+import { devPlugins, devServerconfig, devLoaders } from './config/dev.config';
+import { prodPlugins, prodLoaders } from './config/prod.config';
 
 const ENV = process.env.NODE_ENV || 'development';
 
@@ -15,7 +15,8 @@ module.exports = {
     bundle: [
       'react-hot-loader/patch',
       './src/index.js'
-    ]
+    ],
+    raven: './node_modules/raven-js/dist/raven.js'
   },
 
   output: {
