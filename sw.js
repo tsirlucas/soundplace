f4de0be3e672ae8360d876dde43432d59c75d7a7
Travis CI User <travis@example.org>
2017-10-21 18:15:02 +0000

    Updates

--- sw.js
+++ sw.js
@@ -289,7 +289,7 @@ toolbox.router.get(/^https:\/\/scontent\.xx\.fbcdn\.net\//, toolbox.cacheFirst, 
 toolbox.router.get(/^https:\/\/mosaic\.scdn\.co\//, toolbox.cacheFirst, {});
 toolbox.router.get(/^https:\/\/i\.scdn\.co\//, toolbox.cacheFirst, {});
 toolbox.router.get(/^https:\/\/pl\.scdn\.co\//, toolbox.cacheFirst, {});
-toolbox.router.get(/^https:\/\/youtube-cacheable-audio-stream\.herokuapp\.com\/getAudioStream\//, toolbox.cacheFirst, {"successResponses":200});
+toolbox.router.get(/^https:\/\/youtube-cacheable-audio-stream\.herokuapp\.com\/getAudioStream\//, toolbox.cacheFirst, {"successResponses":/20[01]/});
 
 
 
