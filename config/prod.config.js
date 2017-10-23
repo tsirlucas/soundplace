0f616bb91d1598300c0218d8ee4e3ccedebb68ee
lucascorreia <tsirlucas@gmail.com>
2017-10-23 20:17:08 -0300

    Ignoring report url from sw

--- config/prod.config.js
+++ config/prod.config.js
@@ -48,7 +48,7 @@ export const prodPlugins = [
     filename: 'sw.js',
     // importScripts: ['./service-worker.js'], only if script changes are necessary
     navigateFallback: 'index.html',
-    ignoreUrlParametersMatching: [/^https:\/\/www.\soundplace\.io\/assets\/report.html/],
+    ignoreUrlParametersMatching: [/report\.html/],
     staticFileGlobsIgnorePatterns: [/\.map$/, /\.DS_Store/],
     options: {
       cacheId: 'soundplace'
