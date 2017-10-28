cfa6d21d03fb6a02d2608609d8ec3c483a2db955
tsirlucas <tsirlucas@gmail.com>
2017-10-28 18:06:32 -0300

    Cleaner travis.yml

---
+++ scripts/run_on_deploy.sh
@@ -0,0 +1,19 @@
+set -euo pipefail
+
+gh-pages -d build
+
+echo -e "\nPurging Cloudflare cache...\n"
+
+curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE/purge_cache"
+    -H "X-Auth-Email: tsirlucas@gmail.com"
+    -H "X-Auth-Key: $CLOUDFLARE_TOKEN"
+    -H "Content-Type: application/json"
+    --data "{\"purge_everything\":true}"
+
+echo -e "\nCloudflare cache purged!\n"
+
+cd build
+lighthouse-badges --urls https://www.soundplace.io/ --save-report
+mv www_soundplace_io_.html ./assets/report.html
+cd ..
+gh-pages -d build;
---
+++ scripts/run_pr_tests.sh
@@ -0,0 +1,9 @@
+set -euo pipefail
+
+node_modules/now/download/dist/now switch soundplace --token $NOW_TOKEN
+
+URL=$(node_modules/now/download/dist/now ./build -n prs-soundplace --token $NOW_TOKEN --static --public -f)
+
+echo -e "\nRunning lighthouse tests on: $URL\n"
+
+node ./node_modules/lighthouse-ci/runlighthouse.js $URL --runner=chrome --score=100; fi'
