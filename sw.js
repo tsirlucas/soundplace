916024dfc4962cf7d052a4aa4f5d60c0c94a05b4
tsirlucas <tsirlucas@gmail.com>
2018-02-26 03:58:33 -0300

    Updates

--- sw.js
+++ sw.js
@@ -20,11 +20,11 @@ importScripts('workbox-sw.prod.v2.1.2.js');
 const fileManifest = [
   {
     "url": "/200.html",
-    "revision": "52caf4ac5cf6ab80c5aecc5d68adb589"
+    "revision": "6371f616529dbe63958edfd4d78187a1"
   },
   {
     "url": "/404.html",
-    "revision": "52caf4ac5cf6ab80c5aecc5d68adb589"
+    "revision": "6371f616529dbe63958edfd4d78187a1"
   },
   {
     "url": "assets/report.html",
@@ -32,11 +32,11 @@ const fileManifest = [
   },
   {
     "url": "/bundle.js",
-    "revision": "cc89eae5d3b8aaa2b08c8dceae35c76f"
+    "revision": "190d0c82afafa9cbc07b1cf48be068ab"
   },
   {
     "url": "/index.html",
-    "revision": "52caf4ac5cf6ab80c5aecc5d68adb589"
+    "revision": "6371f616529dbe63958edfd4d78187a1"
   },
   {
     "url": "/raven.min.js",
