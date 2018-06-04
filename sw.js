21e21ed15ac90b4909ec60fba4445baebdcfa4d5
tsirlucas <tsirlucas@gmail.com>
2018-06-03 22:48:08 -0300

    Updates

--- sw.js
+++ sw.js
@@ -1,40 +1,132 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
-
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
-
-importScripts(
-  "precache-manifest.732dd64ced1de230cd8e945962c8eda5.js"
-);
-
-workbox.skipWaiting();
-workbox.clientsClaim();
+importScripts('workbox-sw.prod.v2.1.3.js');
 
 /**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
+ * DO NOT EDIT THE FILE MANIFEST ENTRY
+ *
+ * The method precache() does the following:
+ * 1. Cache URLs in the manifest to a local cache.
+ * 2. When a network request is made for any of these URLs the response
+ *    will ALWAYS comes from the cache, NEVER the network.
+ * 3. When the service worker changes ONLY assets with a revision change are
+ *    updated, old cache entries are left as is.
+ *
+ * By changing the file manifest manually, your users may end up not receiving
+ * new versions of files because the revision hasn't changed.
+ *
+ * Please use workbox-build or some other tool / approach to generate the file
+ * manifest which accounts for changes to local files and update the revision
+ * accordingly.
  */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {
-  "directoryIndex": "./index.html"
+const fileManifest = [
+  {
+    "url": "200.html",
+    "revision": "c3e592934c2cb962806f7b5aae0e1a8d"
+  },
+  {
+    "url": "404.html",
+    "revision": "c3e592934c2cb962806f7b5aae0e1a8d"
+  },
+  {
+    "url": "assets/report.html",
+    "revision": "07fff4d1e6803085aa562628971f98b3"
+  },
+  {
+    "url": "bundle.js",
+    "revision": "8cf3970c9ed38c1fc061a4da821f6d91"
+  },
+  {
+    "url": "index.html",
+    "revision": "c3e592934c2cb962806f7b5aae0e1a8d"
+  },
+  {
+    "url": "raven.min.js",
+    "revision": "452716b418a89a84ccfd77be827204e1"
+  },
+  {
+    "url": "style.css",
+    "revision": "f10be361fe04a17fc0ef495f876c7b70"
+  }
+];
+
+const workboxSW = new self.WorkboxSW({
+  "skipWaiting": true,
+  "clientsClaim": true,
+  "directoryIndex": "index.html"
 });
-
-workbox.routing.registerNavigationRoute("./index.html");
-
-workbox.routing.registerRoute(/^https:\/\/scontent\.xx\.fbcdn\.net\//, workbox.strategies.cacheFirst({ cacheName: "scontent-cache", plugins: [new workbox.expiration.Plugin({"maxEntries":200,"maxAgeSeconds":31536000}), new workbox.cacheableResponse.Plugin({"statuses":[0,200,201,301,304,302]})] }), 'GET');
-workbox.routing.registerRoute(/^https:\/\/mosaic\.scdn\.co\//, workbox.strategies.cacheFirst({ cacheName: "mosaic-cache", plugins: [new workbox.expiration.Plugin({"maxEntries":200,"maxAgeSeconds":31536000}), new workbox.cacheableResponse.Plugin({"statuses":[0,200,201,301,304,302]})] }), 'GET');
-workbox.routing.registerRoute(/^https:\/\/i\.scdn\.co\//, workbox.strategies.cacheFirst({ cacheName: "iscdn-cache", plugins: [new workbox.expiration.Plugin({"maxEntries":200,"maxAgeSeconds":31536000}), new workbox.cacheableResponse.Plugin({"statuses":[0,200,201,301,304,302]})] }), 'GET');
-workbox.routing.registerRoute(/^https:\/\/pl\.scdn\.co\//, workbox.strategies.cacheFirst({ cacheName: "plscdn-cache", plugins: [new workbox.expiration.Plugin({"maxEntries":200,"maxAgeSeconds":31536000}), new workbox.cacheableResponse.Plugin({"statuses":[0,200,201,301,304,302]})] }), 'GET');
-workbox.routing.registerRoute(/^https:\/\/youtube-cacheable-audio-stream\.herokuapp\.com\//, workbox.strategies.cacheFirst({ cacheName: "stream-cache", plugins: [new workbox.expiration.Plugin({"maxEntries":200,"maxAgeSeconds":31536000}), new workbox.cacheableResponse.Plugin({"statuses":[200,201]})] }), 'GET');
+workboxSW.precache(fileManifest);
+workboxSW.router.registerNavigationRoute("index.html");workboxSW.router.registerRoute(/^https:\/\/scontent\.xx\.fbcdn\.net\//, workboxSW.strategies.cacheFirst({
+  "cacheName": "scontent-cache",
+  "cacheableResponse": {
+    "statuses": [
+      0,
+      200,
+      201,
+      301,
+      304,
+      302
+    ]
+  },
+  "cacheExpiration": {
+    "maxAgeSeconds": 31536000
+  }
+}), 'GET');
+workboxSW.router.registerRoute(/^https:\/\/mosaic\.scdn\.co\//, workboxSW.strategies.cacheFirst({
+  "cacheName": "mosaic-cache",
+  "cacheableResponse": {
+    "statuses": [
+      0,
+      200,
+      201,
+      301,
+      304,
+      302
+    ]
+  },
+  "cacheExpiration": {
+    "maxAgeSeconds": 31536000
+  }
+}), 'GET');
+workboxSW.router.registerRoute(/^https:\/\/i\.scdn\.co\//, workboxSW.strategies.cacheFirst({
+  "cacheName": "iscdn-cache",
+  "cacheableResponse": {
+    "statuses": [
+      0,
+      200,
+      201,
+      301,
+      304,
+      302
+    ]
+  },
+  "cacheExpiration": {
+    "maxAgeSeconds": 31536000
+  }
+}), 'GET');
+workboxSW.router.registerRoute(/^https:\/\/pl\.scdn\.co\//, workboxSW.strategies.cacheFirst({
+  "cacheName": "plscdn-cache",
+  "cacheableResponse": {
+    "statuses": [
+      0,
+      200,
+      201,
+      301,
+      304,
+      302
+    ]
+  },
+  "cacheExpiration": {
+    "maxAgeSeconds": 31536000
+  }
+}), 'GET');
+workboxSW.router.registerRoute(/^https:\/\/youtube-cacheable-audio-stream\.herokuapp\.com\//, workboxSW.strategies.cacheFirst({
+  "cacheName": "stream-cache",
+  "cacheableResponse": {
+    "statuses": [
+      200,
+      201
+    ]
+  },
+  "cacheExpiration": {
+    "maxAgeSeconds": 31536000
+  }
+}), 'GET');
